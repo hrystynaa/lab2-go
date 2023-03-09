7fc981ca2c56cbad550d451915e134a16b9b8ade
hrystynaa <yaskulskahrystyna@gmail.com>
2023-03-09 18:55:39 +0200

    Add implentation PrefixToPostfix function

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	lab2 "github.com/roman-mazur/architecture-lab-2"
+
+	lab2 "github.com/hrystynaa/lab2-go"
 )
 
 var (
--- implementation.go
+++ implementation.go
@@ -1,9 +1,37 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
 func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+	stack := []string{}
+	tokens := strings.Split(input, " ")
+	for i := len(tokens) - 1; i >= 0; i-- {
+		token := tokens[i]
+		switch {
+		case isOperator(token):
+			if len(stack) < 2 {
+				return "", fmt.Errorf("invalid prefix expression")
+			}
+			number1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			number2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			postfix := number1 + " " + number2 + " " + token
+			stack = append(stack, postfix)
+		default:
+			stack = append(stack, token)
+		}
+	}
+	if len(stack) != 1 {
+		return "", fmt.Errorf("invalid prefix expression")
+	}
+	return stack[0], nil
+}
+
+// isOperator returns true if the given token is an operator.
+func isOperator(token string) bool {
+	return token == "+" || token == "-" || token == "/" || token == "*" || token == "^"
 }
