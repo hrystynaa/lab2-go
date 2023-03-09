87903c16784b8bec18e6dfa6a9766761d8cfe0ad
hrystynaa <yaskulskahrystyna@gmail.com>
2023-03-09 21:16:43 +0200

    Fix PrefixToPostfix function

--- implementation.go
+++ implementation.go
@@ -8,6 +8,7 @@ import (
 func PrefixToPostfix(input string) (string, error) {
 	stack := []string{}
 	tokens := strings.Fields(input)
+	tokenWasNumber := false
 	for i := len(tokens) - 1; i >= 0; i-- {
 		token := tokens[i]
 		switch {
@@ -15,13 +16,20 @@ func PrefixToPostfix(input string) (string, error) {
 			if len(stack) < 2 {
 				return "", fmt.Errorf("invalid expression: %s", input)
 			}
+			postfix := ""
 			number1 := stack[len(stack)-1]
 			number2 := stack[len(stack)-2]
 			stack = stack[:len(stack)-2]
-			postfix := number1 + " " + number2 + " " + token
+			if tokenWasNumber && len(stack) == 0 {
+				postfix = number2 + " " + number1 + " " + token
+			} else {
+				postfix = number1 + " " + number2 + " " + token
+			}
 			stack = append(stack, postfix)
+			tokenWasNumber = false
 		case isNumber(token):
 			stack = append(stack, token)
+			tokenWasNumber = true
 		default:
 			return "", fmt.Errorf("invalid token: %s", token)
 		}
