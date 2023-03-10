788faf29033e72dcfef9fd4719b575d769cb21b2
vita133 <vitandrukhiv@gmail.com>
2023-03-10 17:51:50 +0200

    make tests with 'gocheck' library

--- implementation_test.go
+++ implementation_test.go
@@ -4,20 +4,38 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "gopkg.in/check.v1"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+func TestPrefixToPostfix(t *testing.T) { TestingT(t) }
+
+type PrefixToPostfixSuite struct{}
+
+var _ = Suite(&PrefixToPostfixSuite{})
+
+func (s *PrefixToPostfixSuite) TestPrefixToPostfix(c *C) {
+	tests := []struct {
+		name   string
+		input  string
+		output string
+		err    error
+	}{
+		{"simple", "+ 1 2", "1 2 +", nil},
+		{"middle", "+ 5 * - 4 2 3", "4 2 - 3 * 5 +", nil},
+	}
+
+	for _, test := range tests {
+		output, err := PrefixToPostfix(test.input)
+
+		c.Check(output, Equals, test.output)
+		c.Check(err, DeepEquals, test.err)
 	}
 }
 
 func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+	res, _ := PrefixToPostfix("- + 2 2 * 9 2")
 	fmt.Println(res)
 
 	// Output:
-	// 2 2 +
+	// 2 2 + 9 2 * -
 }
