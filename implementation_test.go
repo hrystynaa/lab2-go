c34df434c81c34d77361df7b742d68a20a63f7fa
hrystynaa <yaskulskahrystyna@gmail.com>
2023-03-10 17:56:08 +0200

    Add test cases and status output

--- implementation_test.go
+++ implementation_test.go
@@ -22,13 +22,28 @@ func (s *PrefixToPostfixSuite) TestPrefixToPostfix(c *C) {
 	}{
 		{"simple", "+ 1 2", "1 2 +", nil},
 		{"middle", "+ 5 * - 4 2 3", "4 2 - 3 * 5 +", nil},
+		{"complex", "+ / ^ * + 6 7 8 - 8 9 6 * / 2 3 4", "6 7 + 8 * 8 9 - ^ 6 / 2 3 / 4 * +", nil},
+		{"empty", "", "", fmt.Errorf("invalid expression: ")},
+		{"not enouph numbers", "+ 4", "", fmt.Errorf("invalid expression: + 4")},
+		{"postfix", "6 8 ^", "", fmt.Errorf("invalid expression: 6 8 ^")},
+		{"too many operators", "- + 5 * - 4 2 3", "", fmt.Errorf("invalid expression: - + 5 * - 4 2 3")},
+		{"invalid token", "? + 4 * 8 9 6", "", fmt.Errorf("invalid token: ?")},
 	}
 
 	for _, test := range tests {
+
+		c.Logf("CASE: %s", test.name)
+
 		output, err := PrefixToPostfix(test.input)
 
-		c.Check(output, Equals, test.output)
-		c.Check(err, DeepEquals, test.err)
+		status := c.Check(output, Equals, test.output)
+		status = status && c.Check(err, DeepEquals, test.err)
+
+		if status {
+			c.Logf("PASSED\n")
+		} else {
+			c.Logf("FAILED\n")
+		}
 	}
 }
 
